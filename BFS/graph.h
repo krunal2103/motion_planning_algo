c7d5592efc4204dc279b52554559d9491b0662a5
Krunal Chaudhari <krunal2103@gmail.com>
2019-03-27 18:09:29 -0400

    Added comments for documentation

--- BFS/graph.h
+++ BFS/graph.h
@@ -17,16 +17,39 @@ class Graph
     /**
      * @brief Construct a new Graph object
      * 
-     * @param[in] V Number of vertices
+     * @param V Number of vertices
      */
     Graph(int V) : num_V(V)
     {
         list_data.resize(V);
     }
+
+    /**
+     * @brief Add an edge to the Graph
+     * 
+     * @param src Starting vertex of the edge
+     * @param dest Ending vertex of the edge
+     */
     void addEdge(int src, int dest);
+
+    /**
+     * @brief Displays the whole Graph
+     * 
+     */
     void show();
 
+    /**
+     * @brief Implements Breadth-First-Search on the Graph
+     * 
+     * @param s Starting vertex for the search
+     */
     void BFS(int s);
+
+    /**
+     * @brief Implements Depth-First-Search on the Graph
+     * 
+     * @param s Starting vertex for the search
+     */
     void DFS(int s);
 };
 
