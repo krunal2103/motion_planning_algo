6d4e3c42d15765844edae9ad22cbdfb956fe3f16
Krunal Chaudhari <krunal2103@gmail.com>
2019-03-27 16:46:01 -0400

    Added functions for BFS and DFS

--- BFS/graph.h
+++ BFS/graph.h
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
 class Graph
 {
   private:
     int num_V;
     std::vector<std::vector<int> > list_data;
+
+    void DFS_impl(int s, bool visited[]);
 
   public:
     Graph(int V) : num_V(V)
@@ -14,4 +17,7 @@ class Graph
     }
     void addEdge(int src, int dest);
     void show();
+
+    void BFS(int s);
+    void DFS(int s);
 };
--- BFS/main.cpp
+++ BFS/main.cpp
@@ -10,6 +10,10 @@ int main(){
     graph.addEdge(1,4);
     graph.addEdge(2,3);
     graph.addEdge(3,4);
+    graph.addEdge(4,0);
     graph.show();
+
+    graph.BFS(0);
+    graph.DFS(0);
     return 0;
 }
